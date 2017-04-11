f8863c3078ae5c0acde5325993926b8a1e5ac7a7
ebicastudios <ebicastudios@gmail.com>
2017-04-10 22:19:27 -0500

    Restart
    Restart of the project, making sure systems are as abstract and
    decoupled as possible. Changes for this upload are:
    
    - Added file Assets/Scripts/Primary System/FileManager.cs
    - Added libraries needed for functionality.
    - Added file Assets/Scripts/Primary System/Enums.cs
    - Added in FPATHS enum, defined with a single item SETTINGS

---
+++ RPGQuest/Assets/Scripts/Primary System/Enums.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum FPATHS
+{
+    SETTINGS = 0
+}
+
+//public class Enums : MonoBehaviour {
+    //Intentionally Left Blank    
+//}
--- RPGQuest/Assets/Scripts/StateManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class StateManager : MonoBehaviour {
-
-    public GAMESTATE gameState;
-
-
-}
